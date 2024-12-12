af5fbc5de103c0795d8983d418ee7bdfa9c3ecef
wyswill <www.1151373571@live.com>
2024-12-12 16:40:29 +0800

    chagne

--- src/marco.rs
+++ src/marco.rs
@@ -1,4 +1,25 @@
 ///
+/// 获取 redis_conn
+///
+/// ```
+///let mut rds = redis_conn!().await;
+///
+/// let res: Option<()> = rds.get("key").await.expect("msg");
+///
+/// ```
+///
+#[macro_export]
+macro_rules! redis_conn {
+    () => {
+        async {
+            let rds = REDIS.get().expect("msg");
+            rds.conn.clone()
+        }
+    };
+}
+
+
+///
 /// 获取 transaction
 /// *注意*: 在使用后尽快drop tx，否则在多个函数调用中多次调用 `RB.acquire()`  会卡住
 ///
--- src/marco/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod redis_conn;
-pub mod transaction;
