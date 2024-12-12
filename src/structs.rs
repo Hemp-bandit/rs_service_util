9611806f95bca03f7c3f4995dc423f2be706b958
wyswill <www.1151373571@live.com>
2024-12-12 16:55:05 +0800

    feat: add status

--- src/structs.rs
+++ src/structs.rs
@@ -7,3 +7,21 @@ pub struct RedisLoginData {
     pub name: String,
     pub id: i32,
 }
+
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+#[serde(rename = "Enum")]
+pub enum Status {
+    ACTIVE = 1,
+    DEACTIVE = 0,
+}
+
+impl Status {
+    pub fn from(val: i8) -> Status {
+        match val {
+            0 => Status::DEACTIVE,
+            1 => Status::ACTIVE,
+            _ => Status::DEACTIVE,
+        }
+    }
+}
