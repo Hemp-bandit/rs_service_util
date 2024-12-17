56a96d4d6f1158a86b004d4a91823eab611be0c0
wyswill <www.1151373571@live.com>
2024-12-17 15:08:38 +0800

    code

--- src/redis.rs
+++ src/redis.rs
@@ -1,23 +1,20 @@
-use redis::{Client, Connection};
-use std::sync::Arc;
+use redis::{aio::MultiplexedConnection, Client};
 
 pub struct RedisTool {
-    pub conn: Arc<Connection>,
+    pub conn: MultiplexedConnection,
 }
 
 impl RedisTool {
     pub async fn new(redis_url: String) -> Self {
         log::info!("redis_url {redis_url}");
         let client = Client::open(redis_url).unwrap(); // not recommended
-        let conn: Result<Connection, redis::RedisError> = client.get_connection();
+        let conn = client.get_multiplexed_tokio_connection().await;
         match conn {
             Err(err) => {
                 let detail = err.detail().unwrap();
                 panic!("redis connection err {detail}");
             }
-            Ok(conn) => RedisTool {
-                conn: Arc::new(conn),
-            },
+            Ok(conn) => RedisTool { conn },
         }
     }
 }
