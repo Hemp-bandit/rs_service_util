1406b047ef84ea4fef6d57630d87e3a0baf546ca
wyswill <www.1151373571@live.com>
2024-12-18 10:15:43 +0800

    change redis to manage

--- src/redis.rs
+++ src/redis.rs
@@ -1,20 +1,14 @@
-use redis::{aio::MultiplexedConnection, Client};
+use redis::{aio::ConnectionManager, Client};
 
 pub struct RedisTool {
-    pub conn: MultiplexedConnection,
+    pub conn: ConnectionManager,
 }
 
 impl RedisTool {
     pub async fn new(redis_url: String) -> Self {
         log::info!("redis_url {redis_url}");
         let client = Client::open(redis_url).unwrap(); // not recommended
-        let conn = client.get_multiplexed_tokio_connection().await;
-        match conn {
-            Err(err) => {
-                let detail = err.detail().unwrap();
-                panic!("redis connection err {detail}");
-            }
-            Ok(conn) => RedisTool { conn },
-        }
+        let manage = client.get_connection_manager().await.expect("msg");
+        RedisTool { conn: manage }
     }
 }
