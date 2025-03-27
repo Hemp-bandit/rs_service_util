d5997a9b1f9011329d99b345098002b091d4a94c
yansongwang-bd <yan.song@blockdance.io>
2025-03-27 11:38:26 +0800

    feat: add redis timeout

--- src/redis.rs
+++ src/redis.rs
@@ -1,4 +1,5 @@
-use redis::{aio::ConnectionManager, Client};
+use std::time::Duration;
+use redis::{aio::{ConnectionManager, ConnectionManagerConfig}, Client};
 
 pub struct RedisTool {
     pub conn: ConnectionManager,
@@ -8,7 +9,9 @@ impl RedisTool {
     pub async fn new(redis_url: String) -> Self {
         log::info!("redis_url {redis_url}");
         let client = Client::open(redis_url).unwrap(); // not recommended
-        let manage = client.get_connection_manager().await.expect("msg");
+        let mut config = ConnectionManagerConfig::new();
+        config =config.set_connection_timeout(Duration::new(30, 0));
+        let manage = client.get_connection_manager_with_config(config).await.expect("msg");
         RedisTool { conn: manage }
     }
 }
